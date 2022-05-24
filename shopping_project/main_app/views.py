479ca8021c078d11947c05b4a4f121cde2edeb52
jbellon <josh@joshbellon.com>
2022-05-24 13:41:11 -0700

    base.html is added and running properly

--- shopping_project/main_app/views.py
+++ shopping_project/main_app/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Store
 
 #### Define the home view
 def home(request):
   return render(request, 'index.html')
+
+def stores_index(request):
+  stores = Store.objects.all()
+  return render(request, 'stores/index.html', {'stores' : stores})
