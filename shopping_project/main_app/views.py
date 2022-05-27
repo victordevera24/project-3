9e012bc16485957e45521834265e4671b469c433
jbellon <josh@joshbellon.com>
2022-05-27 11:29:34 -0700

    Products within a wishlist can be viewed on a wishlist detail page

--- shopping_project/main_app/views.py
+++ shopping_project/main_app/views.py
@@ -155,4 +155,8 @@ def wishlists_index(request):
   wishlists = WishList.objects.filter(users=request.user)
   return render(request, 'wishlists/index.html', {'wishlists' : wishlists, 'user': request.user})
 
-
+@login_required
+def wishlists_detail(request, wishlist_id):
+  wishlist = WishList.objects.get(id=wishlist_id)
+  products = wishlist.products.all()
+  return render(request, 'wishlists/detail.html', {'wishlist' : wishlist, 'products' : products})
