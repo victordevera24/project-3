882625fa62d7f57978cc69dab871f829e51370d5
Hemalatha-kola <kola123hema@gmail.com>
2022-05-30 12:58:26 -0700

    implemented postgressql database

--- shopping_project/shopping_project/settings.py
+++ shopping_project/shopping_project/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'shopping_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'shopping_project',
+        'USER': 'myappuser',
+        'PASSWORD' : 'mypass',
+        'HOST': 'localhost',
+        'PORT' : 5432
     }
 }
 
